28a27d5202bb652c2ba5c4b69b8548cbb0105120
csnaren <csnaren@gmail.com>
2014-01-02 11:44:32 +0530

    pending issues

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -735,4 +735,32 @@ app.filter('timeAgo', ['timeAgo', 'nowTime', function(timeAgo, nowTime) {
 	};
 }]);
 
+app.directive('keyFocus', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, elem, attrs) {
+      elem.bind('keydown', function (e) {
+      	if(e.keyCode >=48 && e.keyCode <= 57) {
+	       if(elem[0].value.length > 2) {
+	       	console.log(elem[0].value+"*************");
+	       	elem[0].nextElementSibling.focus();
+	       }
+       }
+       /* // up arrow
+
+        if (e.keyCode == 38) {
+          if(!scope.$first) {
+            elem[0].previousElementSibling.focus();
+          }
+        }
+        // down arrow
+        else if (e.keyCode == 40) {
+          if(!scope.$last) {
+            elem[0].nextElementSibling.focus();
+          }
+        }*/
+      });
+    }
+  };
+});
 
